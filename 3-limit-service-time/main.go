6928013200c38a32c3b1a78f9ce1189f52322608
Candu Dmitri <d.candu@hotmail.com>
2018-11-16 11:06:49 -0500

    Solved 3th ex

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,18 +10,51 @@
 
 package main
 
+import (
+	"sync"
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
 	ID        int
 	IsPremium bool
 	TimeUsed  int64 // in seconds
+	mut       sync.Mutex
 }
 
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
+	done := make(chan struct{})
+	var timeLast int64
+	if !u.IsPremium {
+		timeLast = 10
+	} else {
+		u.mut.Lock()
+		timeLast = 10 - u.TimeUsed
+		u.mut.Unlock()
+	}
+
+	go func() {
+		start := time.Now()
+		process()
+		complete := time.Since(start)
+		if u.IsPremium {
+			u.mut.Lock()
+			u.TimeUsed += int64(complete)
+			u.mut.Unlock()
+		}
+		done <- struct{}{}
+	}()
+
+	select {
+	case <-time.After((time.Duration)(timeLast) * time.Second):
+		return false
+	case <-done:
+		return true
+	}
 	return true
 }
 
